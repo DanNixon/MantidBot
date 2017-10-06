a781d469141c6ca3d8761825e494481428a76902
Dan Nixon <dan@dan-nixon.com>
2017-10-06 09:41:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(
     name='mantid_pr_bot',
     version='0.1.0',
+    author='Dan Nixon',
+    author_email='daniel.nixon@stfc.ac.uk',
     py_modules=['mantid_pr_bot'],
     install_requires=[
         'Click',
@@ -10,5 +12,11 @@ setup(
     entry_points='''
         [console_scripts]
         mantid_pr_bot=mantid_pr_bot.main:main
-    '''
+    ''',
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux'
+    ]
 )
